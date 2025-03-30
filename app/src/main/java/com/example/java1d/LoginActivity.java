3fff41d9de721f15329cda4f43a58a678e73a082
bendito96 <benedict9696@hotmail.com>
2025-03-31 01:55:14 +0800

    fixed merging error

--- app/src/main/java/com/example/java1d/LoginActivity.java
+++ app/src/main/java/com/example/java1d/LoginActivity.java
@@ -8,26 +8,22 @@ import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 public class LoginActivity extends BackgroundActivity {
-    EditText usernameInput, passwordInput;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
         playMusic(R.raw.background_music);
-        usernameInput = findViewById(R.id.username_input);
-        passwordInput = findViewById(R.id.password_input);
         Button signUpButton = findViewById(R.id.sign_up_button);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
-               startActivity(intent);
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -35,40 +31,8 @@ public class LoginActivity extends BackgroundActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = String.valueOf(usernameInput.getText());
-                String password = String.valueOf(passwordInput.getText().toString());
-
-                DBHelper db_helper = new DBHelper(LoginActivity.this);
-
-                User userExists = db_helper.checkUserExists(username,password);
-
-                if (userExists != null){
-                    Intent newIntent;
-
-                    if(userExists.getClassName().trim().equals("NIL")){
-                        //newIntent = new Intent(LoginActivity.this,AvatarChoosing.class);
-                        //Need to change to intent when activity is added
-                        setContentView(R.layout.hero_selection_page);
-                        //Log.d("Login", "ClassName: '" + userExists.getClassName() + "'");
-                        //System.out.println(userExists.getClassName());
-                    }
-                    else {
-                        //System.out.println(userExists.getClassName());
-                        newIntent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(newIntent);
-                    }
-
-                    // Pass the user details using intent extras
-                    //newIntent.putExtra("uid", userExists.getUserId());
-                    //newIntent.putExtra("username", userExists.getUsername());
-                    //newIntent.putExtra("email", userExists.getEmail());
-                    //newIntent.putExtra("className", userExists.getClassName());
-                    //startActivity(newIntent);
-
-                }
-                else{
-                    Toast.makeText(LoginActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
-                }
+                //Need to change to intent when activity is added
+                setContentView(R.layout.hero_selection_page);
             }
         });
     }
--- app/src/main/java/com/example/java1d/SignUpActivity.java
+++ app/src/main/java/com/example/java1d/SignUpActivity.java
@@ -2,87 +2,22 @@ package com.example.java1d;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignUpActivity extends BackgroundActivity{
-
-    EditText emailInput, passwordInput , repasswordInput,usernameInput;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_page);
         TextView loginText = findViewById(R.id.login_text);
-        emailInput = findViewById(R.id.email_input);
-        usernameInput = findViewById(R.id.username_input);
-        passwordInput = findViewById(R.id.password_input);
-        repasswordInput = findViewById(R.id.confirm_password_input);
-        Button btnReg = findViewById(R.id.create_button);
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
-            }
-        });
-
-
-        btnReg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO 1. need to check if username/email exists in db - if it does, prompt to differ
-
-                String email = emailInput.getText().toString().trim();
-                String password = passwordInput.getText().toString().trim();
-                String rePassword = repasswordInput.getText().toString().trim();
-                String username = usernameInput.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(username)) {
-                    Toast.makeText(SignUpActivity.this, "Enter username", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password) | (TextUtils.isEmpty(rePassword))) {
-                    Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-                if (!password.equals(rePassword)) {
-                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-                //TODO 2 : HASH PASSWORD before storing into DB.
-                //new user has all the game values set to ZERO and className as NIL.
-                User newUser = new User(username, email, password,"NIL",0,0,0,0,null);
-
-
-                DBHelper db_helper = new DBHelper(SignUpActivity.this);
-                long result = db_helper.createUser(newUser);
-                db_helper.close();
-
-                if (result != -1) {
-                    Toast.makeText(SignUpActivity.this, "Success! Registered", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                    startActivity(intent);
-
-                } else {
-                    Toast.makeText(SignUpActivity.this, "Error saving to database", Toast.LENGTH_SHORT).show();
-                }
-
             }
         });
     }
