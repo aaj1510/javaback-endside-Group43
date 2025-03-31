214b210313fee12330f908a89eee6e642f833f1f
Potacchi <Innamouse.singapore@gmail.com>
2025-03-31 15:10:52 +0800

    feat: Added Home Page and Nav Bar
    Added Home Page as its own layout and Navigation Bar into activity main

---
+++ app/src/main/java/com/example/java1d/TaskFragment.java
@@ -0,0 +1,18 @@
+package com.example.java1d;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskFragment extends Fragment {
+   @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tasks_page, container, false);
+    }
+}
