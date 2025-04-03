f6c6f753b30f5ac13b3953118131ac62218da187
bendito96 <benedict9696@hotmail.com>
2025-04-03 19:33:43 +0800

    changed HomeFragment code to allow it to work with Joyce's onClick function

--- app/src/main/java/com/example/java1d/HomeFragment.java
+++ app/src/main/java/com/example/java1d/HomeFragment.java
@@ -4,31 +4,76 @@ import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.home_page, container, false);
-        ImageButton task_button = view.findViewById(R.id.task_button);
-        task_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TaskFragment dialogFragment = new TaskFragment();
-                PresetTasksFragment dialogFragment = new PresetTasksFragment();
-                // dialogFragment.show(getActivity().getSupportFragmentManager(), "TaskFragment");
-                dialogFragment.show(getActivity().getSupportFragmentManager(), "PresetTasksFragment");
-            }
-        });
+        TextView username_text = view.findViewById(R.id.username);
+        //username_text.setText(username);
+        ImageView avatar_image = view.findViewById(R.id.avatar);
 
+        // Buttons
+        ImageButton task_button = view.findViewById(R.id.task_button);
+        ImageButton boss_button = view.findViewById(R.id.boss_button);
+        ImageButton inventory_button = view.findViewById(R.id.inventory_button);
+        ImageButton achievements_button = view.findViewById(R.id.achievement_button);
+
+        // Listeners
+        task_button.setOnClickListener(this);
+        boss_button.setOnClickListener(this);
+        inventory_button.setOnClickListener(this);
+        achievements_button.setOnClickListener(this);
 
         return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.task_button) {
+//            TaskFragment dialogFragment = new TaskFragment();
+//            dialogFragment.show(getActivity().getSupportFragmentManager(), "TaskFragment");
+            PresetTasksFragment dialogFragment = new PresetTasksFragment();
+            dialogFragment.show(getActivity().getSupportFragmentManager(), "PresetTasksFragment");
+        }
+        else if (view.getId() == R.id.boss_button) {
+            replaceFragment(new ViewBossFragment());
+        }
+        else if (view.getId() == R.id.inventory_button) {
+            replaceFragment(new InventoryFragment());
+        }
+        else if (view.getId() == R.id.achievement_button) {
+            replaceFragment(new AchievementsFragment());
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+//        task_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //TaskFragment dialogFragment = new TaskFragment();
+//                PresetTasksFragment dialogFragment = new PresetTasksFragment();
+//                // dialogFragment.show(getActivity().getSupportFragmentManager(), "TaskFragment");
+//                dialogFragment.show(getActivity().getSupportFragmentManager(), "PresetTasksFragment");
+//            }
+//        });
+
 }
